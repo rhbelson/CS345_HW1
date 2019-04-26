26bdba1ffc807bb017a57239b1b5e56e281a2088
shzhangyihan <shzhangyihan@gmail.com>
2019-04-26 18:09:48 -0500

    map being blocked

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
 import "fmt"
+import "sync"
+
+var worker_list = make([]string, 0)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -12,6 +15,7 @@ import "fmt"
 // existing registered workers (if any) and new ones as they register.
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
+    fmt.Println("!!! Schedule is being called !!!")
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
@@ -25,23 +29,47 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
+    var avaliable_worker = make(chan string)
+
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	// Your code here (Part 2, 2B).
 	//If it's a map task (how can we be sure?)
-	for taskNumber := 0; taskNumber < ntasks; taskNumber ++ {
-		//Reading registerChan argument
-		worker_rpc_address := <-registerChan
+    go pull_workers(registerChan, avaliable_worker)
+    var wg sync.WaitGroup
 
-		//RPC's arguments are defined by DoTaskArgs in mapreduce/common_rpc.go
-		task_args:=DoTaskArgs{jobName,mapFiles[taskNumber],mapPhase,taskNumber,n_other}
-
-		//Send RPC to worker
-		go call(worker_rpc_address, "Worker.DoTask",task_args, nil)
-
+    taskNumber := 0
+    for taskNumber < ntasks {
+        worker := <- avaliable_worker
+        if(len(worker) > 0) {
+            task_args := DoTaskArgs{jobName, mapFiles[taskNumber], mapPhase,
+                                    taskNumber, n_other}
+            wg.Add(1)
+            go start_worker(worker, "Worker.DoTask", task_args, nil,
+                            avaliable_worker, &wg)
+            taskNumber = taskNumber + 1
+        }
+    }
+    fmt.Printf("Start waiting taskNum: %d ntasks: %d\n", taskNumber, ntasks)
+    wg.Wait()
+	fmt.Printf("Schedule: %v done\n", phase)
 }
 
+func pull_workers(c chan string, avaliable_worker chan string) {
+  for {
+    worker := <- c
+    worker_list = append(worker_list, worker)
+    fmt.Printf("Putting worker %v in avaliable\n", worker)
+    avaliable_worker <- worker
+  }
+}
 
-
-	fmt.Printf("Schedule: %v done\n", phase)
+func start_worker(worker string, rpcname string, task_args interface{},
+                  reply interface{}, avaliable_worker chan string, wg *sync.WaitGroup) {
+    defer wg.Done()
+    fmt.Printf("Worker %v is assigned\n", worker)
+    fmt.Println(task_args)
+    call(worker, "Worker.DoTask", task_args, reply)
+    fmt.Printf("Putting worker %v in avaliable\n", worker)
+    avaliable_worker <- worker
 }
