f47dcaaeea2a26bb204ce52360868934a42601be
shzhangyihan <shzhangyihan@gmail.com>
2019-04-26 18:33:02 -0500

    phase changed

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -16,21 +16,23 @@ var worker_list = make([]string, 0)
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
     fmt.Println("!!! Schedule is being called !!!")
+    var isMap bool
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
 		n_other = nReduce
+        isMap = true
 	case reducePhase:
 		ntasks = nReduce
 		n_other = len(mapFiles)
+        isMap = false
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
     var avaliable_worker = make(chan string)
-
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	// Your code here (Part 2, 2B).
@@ -42,16 +44,22 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
     for taskNumber < ntasks {
         worker := <- avaliable_worker
         if(len(worker) > 0) {
-            task_args := DoTaskArgs{jobName, mapFiles[taskNumber], mapPhase,
-                                    taskNumber, n_other}
+            var task_args interface{}
+            if(isMap) {
+                task_args = DoTaskArgs{jobName, mapFiles[taskNumber], mapPhase, taskNumber, n_other}
+            } else {
+                task_args = DoTaskArgs{jobName, "", reducePhase, taskNumber, n_other}
+            }
+
             wg.Add(1)
+            fmt.Println(task_args)
             go start_worker(worker, "Worker.DoTask", task_args, nil,
                             avaliable_worker, &wg)
             taskNumber = taskNumber + 1
         }
     }
-    fmt.Printf("Start waiting taskNum: %d ntasks: %d\n", taskNumber, ntasks)
-    wg.Wait()
+    fmt.Printf("Start waiting taskNum: %d\n", taskNumber)
+    //wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
 
@@ -68,8 +76,7 @@ func start_worker(worker string, rpcname string, task_args interface{},
                   reply interface{}, avaliable_worker chan string, wg *sync.WaitGroup) {
     defer wg.Done()
     fmt.Printf("Worker %v is assigned\n", worker)
-    fmt.Println(task_args)
     call(worker, "Worker.DoTask", task_args, reply)
-    fmt.Printf("Putting worker %v in avaliable\n", worker)
+    fmt.Printf("Putting worker %v in avaliable  ", worker)
     avaliable_worker <- worker
 }
